74afd74c480f7de92e1804a7fc3f15057f926728
Daniel Gladstone <816000924@saelec.uwi.tt>
2018-11-07 19:10:48 -0400

    Keypad completed

---
+++ Source/keyrename.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   keyrename.h
+ * Author: 816000924
+ *
+ * Created on November 7, 2018, 3:05 PM
+ */
+
+#ifndef KEYRENAME_H
+#define	KEYRENAME_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#define k0 PORTBits.RB7
+#define k1 PORTBits.RB6
+#define k2 PORTBits.RB4
+#define k3 PORTBits.RB3    
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* KEYRENAME_H */
+
